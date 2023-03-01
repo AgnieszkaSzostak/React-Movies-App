9c2b292dd1cf78eaff85cc9f00a27c6b0270934b
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-03-01 11:29:24 +0100

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import PageHome from './pages/PageHome/PageHome';
 import PageMoviesSearch from './pages/PageMoviesSearch/PageMoviesSearch';
+import { Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <PageHome/>
-      <PageMoviesSearch/>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies/search">Movies search</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path={"/"} element={<PageHome/>}></Route>
+        <Route path={"/movies/search"} element={<PageMoviesSearch/>}></Route>
+      </Routes>
     </div>
   );
 }
